47bb7dedbddabebc7027110463188194c3678f7e
yejune <kwon@yejune.com>
2017-02-27 18:36:40 +0900

    update phalcon compiler config

--- files/usr/local/bin/installer/phalcon.sh
+++ files/usr/local/bin/installer/phalcon.sh
@@ -9,9 +9,15 @@ wget https://github.com/phalcon/cphalcon/archive/v${PHALCON_VER}.tar.gz
 
 tar zxvf v${PHALCON_VER}.tar.gz
 
-cd /usr/src/pecl/cphalcon-${PHALCON_VER}/build
+cd /usr/src/pecl/cphalcon-${PHALCON_VER}/build/php7/64bits
 
-./install
+phpize
+
+./configure CFLAGS="-O2 -g -fomit-frame-pointer -DPHALCON_RELEASE"
+
+make
+
+make install
 
 echo "extension=phalcon.so" > ${PHP_INI_DIR}/conf.d/phalcon.ini
 
