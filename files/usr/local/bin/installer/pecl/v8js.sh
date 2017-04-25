92c178ac872dd33b351499b68dd989e3cf7dd5df
yejune <kwon@yejune.com>
2017-04-25 19:00:11 +0900

    change v8,v8js git to pecl

--- files/usr/local/bin/installer/pecl/v8js.sh
+++ files/usr/local/bin/installer/pecl/v8js.sh
@@ -8,8 +8,4 @@ apt-add-repository ppa:pinepain/libv8-${LIBV8_VERSION} -y
 apt-get update && apt-get install -y --no-install-recommends \
         libv8-${LIBV8_VERSION}-dev -y --allow-unauthenticated
 
-mkdir -p /usr/src/pecl
-
-cd /usr/src/pecl
-
-git clone -b php7 https://github.com/phpv8/v8js ${lib}
+pecl ${lib}
