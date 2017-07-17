3c54630799896859f75974c9da5f9b8e467a6993
yejune <kwon@yejune.com>
2017-07-17 15:06:08 +0900

    memcached.sh cleanup

--- files/usr/local/bin/installer/pecl/memcached.sh
+++ files/usr/local/bin/installer/pecl/memcached.sh
@@ -6,6 +6,6 @@ set -e
 apt-get update && apt-get install -y --no-install-recommends --allow-unauthenticated \
         libmemcached-dev
 
-git clone -b php7 https://github.com/php-memcached-dev/php-memcached ${lib}
+#git clone -b php7 https://github.com/php-memcached-dev/php-memcached ${lib}
 
 pecl ${lib}
