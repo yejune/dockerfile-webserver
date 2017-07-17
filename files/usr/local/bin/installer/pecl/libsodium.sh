2b850b2e7c734e7b25c1adaa2ae2ff2f5e75108d
yejune <kwon@yejune.com>
2017-07-17 15:06:31 +0900

    libsodium version up stable

--- files/usr/local/bin/installer/pecl/libsodium.sh
+++ files/usr/local/bin/installer/pecl/libsodium.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -e
 
-apt-get update && apt-get install -y --no-install-recommends \
-        libsodium-dev
+#apt-get update && apt-get install -y --no-install-recommends \
+#        libsodium-dev
+
+mkdir -p /usr/src/pecl
+
+cd /usr/src/pecl
+
+git clone https://github.com/jedisct1/libsodium --branch stable
+
+cd libsodium
+
+./configure
+
+make && make install
 
 pecl ${lib}
